fd276bfdde494a14b7b6b5a7fd58d7678502c84a
Ben <benfl3713@gmail.com>
2025-02-19 20:49:07 +0000

    Add web history support

--- lib/services/database_service.dart
+++ lib/services/database_service.dart
@@ -1,13 +1,23 @@
 import 'package:sqflite/sqflite.dart';
+import 'package:sqflite_common_ffi_web/sqflite_ffi_web.dart';
 import 'package:path/path.dart';
 import 'package:pooker_score/models/game_result.dart';
+import 'package:flutter/foundation.dart';
 
 class GameDatabaseService {
   static Database? _database;
 
   static Future<void> initDatabase() async {
+    String path;
+    if (kIsWeb) {
+      databaseFactory = databaseFactoryFfiWeb;
+      path = 'pooker.db';
+    } else {
+      path = join(await getDatabasesPath(), 'pooker.db');
+    }
+
     _database = await openDatabase(
-      join(await getDatabasesPath(), 'pooker.db'),
+      path,
       onCreate: (db, version) {
         return db.execute(
           'CREATE TABLE game_history(id INTEGER PRIMARY KEY, date TEXT, players TEXT)',
